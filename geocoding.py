124c5b7a6b3438ecf251e7393043283252206c03
Matt Carey <matt@authorityspoke.com>
2020-04-19 21:21:30 -0500

    Check if geocode is likely by comparing zip codes.

--- geocoding.py
+++ geocoding.py
@@ -1,6 +1,6 @@
 import csv
 import os
-from typing import Dict, List, Tuple
+from typing import Dict, Iterator, List, Optional, Tuple
 
 
 from dotenv import load_dotenv
@@ -19,29 +19,36 @@ def collect_rows() -> List[Dict[str, str]]:
 
 
 def make_full_address(row: Dict[str, str]):
-    return f'{row["Address"]}, {row["City "]}, {row["State"]} {row["Zip"]}'
+    return f'{row["PropertyName"]}, {row["Address"]}, {row["City "]}, {row["State"]}'
 
 
 def request_geocode(row) -> Dict:
     full_address = make_full_address(row)
     response = requests.get(
         "http://www.mapquestapi.com/geocoding/v1/address",
-        params={"key": MAPQUEST_KEY, "location": full_address, "maxResults": 1},
+        params={"key": MAPQUEST_KEY, "location": full_address, "maxResults": 5},
     ).json()
     return response
 
 
+def get_likely_geocodes(row) -> Optional[Dict]:
+    """Accept only the geocodes that get the zip code right."""
+    response = request_geocode(row)
+    for location in response["results"][0]["locations"]:
+        if location["postalCode"][:5] == row["Zip"][:5]:
+            return location
+    return None
+
+
 def coordinates_changed(row: Dict, lat: float, lng: float) -> bool:
-    if abs(row["Latitude"] - lat) > 0.01:
+    if abs(float(row["Latitude"]) - lat) > 0.01:
         return True
-    if abs(row["Longitude"] - lng) > 0.01:
+    if abs(float(row["Longitude"]) - lng) > 0.01:
         return True
     return False
 
 
-def get_coordinates(row) -> Tuple[float, float]:
-    response = request_geocode(row)
-    result = response["results"][0]
-    lat = result["locations"][0]["latLng"]["lat"]
-    lng = result["locations"][0]["latLng"]["lng"]
+def get_coordinates(location: Dict) -> Tuple[float, float]:
+    lat = location["latLng"]["lat"]
+    lng = location["latLng"]["lng"]
     return lat, lng
--- test_geocode.py
+++ test_geocode.py
@@ -10,8 +10,8 @@ class TestGeocoding:
 
     def test_make_address_from_row(self, addresses):
         row = addresses[0]
-        full_address = "5250 Town and Country Boulevard, Frisco, TX 75034"
-        assert geocoding.make_full_address(row) == full_address
+        full_address = "5250 Town and Country Boulevard, Frisco, TX"
+        assert full_address in geocoding.make_full_address(row)
 
     @pytest.mark.vcr()
     def test_get_coordinates_for_address(self, addresses):
@@ -21,3 +21,17 @@ class TestGeocoding:
         # 32.378061, -96.166077
         assert 32 < latitude < 33
         assert -97 < longitude < -96
+
+    @pytest.mark.vcr()
+    def test_get_likely_geocodes(self, addresses):
+        row = addresses[2]
+        assert row["PropertyName"] == "SPHINX AT MURDEAUX VILLAS"
+        likely = [x for x in geocoding.get_likely_geocodes(row)]
+        assert likely[0] == "Austin"
+
+    def test_coordinates_changed(self, addresses):
+        row = addresses[0]
+        lat = 33.088442
+        lng = -96.84384
+        changed = geocoding.coordinates_changed(row, lat, lng)
+        assert changed is True
